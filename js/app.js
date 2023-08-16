3e0d52e5533f6e705775bcea2cf8eb273a851313
christian burns <christianburns03@gmail.com>
2023-08-16 16:07:03 -0300

    hora 18

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 const hamburguesa = document.querySelector('.hamburguesa');
 const navegacion = document.querySelector('.navegacion');
+const enlaces = document.querySelectorAll('.navegacion a');
+
 
 document.addEventListener('DOMContentLoaded', ()=>{
     mostrarMenu();
-})
+    cerrarMenu();
+});
 
 function mostrarMenu(){
-    hamburguesa.addEventListener('click', ()=>{
+    hamburguesa.addEventListener('click',()=> {
         navegacion.classList.toggle('ocultar');
-    })
+    });
+}
+
+function cerrarMenu(){
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click',(e)=>{
+            
+            if(e.target.tagName === 'A'){
+                navegacion.classList.add('ocultar');
+            }
+        });
+    });
 }
